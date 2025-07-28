54ee420e9cc862dcce7d18396a0797219cfce0bb
Manthan Sorkhade <sorkhademanthan@gmail.com>
2025-07-28 16:53:16 +0530

    Initial commit

---
+++ car-rental-app/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cars from "./pages/Cars";
+import CarDetails from "./pages/CarDetails";
+import Booking from "./pages/Booking";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Navbar />
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
